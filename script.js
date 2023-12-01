debacd7da3adb4a57773b4539550dd35c41d5ce3
EhtiramMaharramli <95569569+EhtiramMaharramli@users.noreply.github.com>
2023-12-01 23:47:03 +0400

    Adding search for title

--- script.js
+++ script.js
@@ -1,44 +1,49 @@
+let dummy_products = [];
+
 function fetchData() {
-  
   fetch("https://dummyjson.com/products/?limit=100")
     .then(response => {
       if (!response.ok) {
-        throw Error('There is error. You need to fix this!');
+        throw Error('There is an error. You need to fix this!');
       }
-
       return response.json();
     })
     .then(data => {
-      console.log(data);
-
-      if (data && data.products && Array.isArray(data.products)) {
-        displayProducts(data.products);
-      } else {
-        console.error('Invalid data structure:', data);
-      }
+      dummy_products = data.products; 
+      displayProducts(dummy_products);
     })
     .catch(error => {
       console.error(error);
     });
 }
 
-
 function displayProducts(products) {
   const html = products.map(product => `
     <div class="products">
-      <h3>Title: ${product.title}</h3>
+      <h3>${product.title}</h3>
       <p>Price: ${product.price}$</p>
       <p>Discount: ${product.discountPercentage}%</p>
       <p>Category: ${product.category}</p>
       <p>Stock: ${product.stock}</p>
       <img src="${product.thumbnail}" alt="Thumbnail" class="thumbnail">
+      <button onclick="ToDetails(${product.id})" class="btn">More Details</button>
     </div>`
   ).join("");
 
   document.querySelector('#pr').innerHTML = html;
 }
 
+function ToDetails(productId) {
+  window.location.href = `pr_details.html?id=${productId}`;
+}
 
-
+const search = document.getElementById('search');
+search.addEventListener('input', (e) => { 
+  const searchStr = e.target.value.toLowerCase(); 
+  const filteredProducts = dummy_products.filter(product => {
+    return product.title.toLowerCase().includes(searchStr);
+  });
+  displayProducts(filteredProducts);
+});
 
 fetchData();
